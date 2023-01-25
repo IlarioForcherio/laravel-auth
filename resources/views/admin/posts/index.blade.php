0ae5c9385d888e30461f0215f09d6faa4b2a9e50
ilarioforcherio <ilarioforcherio@gmail.com>
2023-01-25 17:26:21 +0100

    create

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,19 +4,22 @@
 @section('content')
 <div class="text-center" >
  <h1>Pagina dei post</h1>
+
+ {{-- Funzione Create--}}
+ <a href="admin.posts.create" class="btn btn-warning mt-4">Crea un nuovo Post</a>
 </div>
 
 
 @foreach ($posts as $item)
 
-<div class="d-flex justify-content-center" >
+<div class="d-flex justify-content-center mt-4 text-center" >
    <div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">{{$item->title}}</h5>
       <p class="card-text">{{$item->id}}</p>
       <p class="card-text">{{$item->body}}</p>
-      <a href="{{route('admin.posts.show', $item->id)}}" class="btn btn-primary">Visualizza Post</a>
+      <a href="{{route('admin.posts.show', $item->id)}}" class="btn btn-primary ">Visualizza Post</a>
     </div>
   </div>
 </div>
